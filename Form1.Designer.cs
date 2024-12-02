9adb4e51c4f584ffd95f6f8d95b99b4526794dcb
Reytler Souza <reytlercompare@hotmail.com>
2024-12-02 17:01:05 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServiceName = new System.Windows.Forms.TextBox();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.lstChekedBoxServices = new System.Windows.Forms.CheckedListBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServiceName
+            // 
+            this.txtServiceName.Location = new System.Drawing.Point(253, 12);
+            this.txtServiceName.Name = "txtServiceName";
+            this.txtServiceName.Size = new System.Drawing.Size(266, 20);
+            this.txtServiceName.TabIndex = 0;
+            this.txtServiceName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(525, 10);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 2;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(24, 270);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(75, 23);
+            this.btnParar.TabIndex = 3;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(117, 270);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 4;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(214, 270);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 5;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // lstChekedBoxServices
+            // 
+            this.lstChekedBoxServices.FormattingEnabled = true;
+            this.lstChekedBoxServices.Location = new System.Drawing.Point(12, 60);
+            this.lstChekedBoxServices.Name = "lstChekedBoxServices";
+            this.lstChekedBoxServices.Size = new System.Drawing.Size(776, 184);
+            this.lstChekedBoxServices.TabIndex = 6;
+            this.lstChekedBoxServices.SelectedIndexChanged += new System.EventHandler(this.checkedListBox_SelectedIndexChanged);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(617, 10);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lstChekedBoxServices);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.txtServiceName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServiceName;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.CheckedListBox lstChekedBoxServices;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using System.ServiceProcess;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        ServiceController[] services;
+        static string selectedService = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CheckedListBox checkedListBox = (CheckedListBox)sender;
+            int selectedIndex = checkedListBox.SelectedIndex;
+            selectedService = services[selectedIndex].ServiceName;
+        }
+
+        private void limparServicoSelecionado()
+        {
+            selectedService = "";
+        }
+
+        private void listarServicos()
+        {
+            services = ServiceController.GetServices().OrderBy(service => service.ServiceName).ToArray();
+            lstChekedBoxServices.Items.Clear();
+            if (string.IsNullOrEmpty(txtServiceName.Text.Trim()))
+            {
+                foreach (ServiceController service in services)
+                {
+                    lstChekedBoxServices.Items.Add($"{service.ServiceName} - {service.DisplayName} - {service.Status}");
+                }
+            }
+            else
+            {
+
+                services = services.Where(service => service.ServiceName.IndexOf(txtServiceName.Text, StringComparison.OrdinalIgnoreCase) >= 0).ToArray();
+                foreach (ServiceController service in services)
+                {
+                    lstChekedBoxServices.Items.Add($"{service.ServiceName} - {service.DisplayName} - {service.Status}");
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtServiceName.Text = "";
+                listarServicos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao listar serviços: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            try {
+                listarServicos();
+                limparServicoSelecionado();
+            }
+            catch (Exception ex){
+                MessageBox.Show($"Erro ao listar serviços: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serviceName = selectedService;
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    MessageBox.Show("Por favor, insira o nome do serviço.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                ServiceController service = new ServiceController(serviceName);
+                
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                    MessageBox.Show($"Serviço '{serviceName}' parado com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"O serviço '{serviceName}' não está em execução.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                listarServicos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao parar o serviço: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serviceName = selectedService;
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    MessageBox.Show("Por favor, insira o nome do serviço.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Stopped)
+                {
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running);
+                    MessageBox.Show($"Serviço '{serviceName}' iniciado com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"O serviço '{serviceName}' já está em execução.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                listarServicos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao iniciar o serviço: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serviceName = selectedService;
+                if (string.IsNullOrEmpty(serviceName))
+                {
+                    MessageBox.Show("Por favor, insira o nome do serviço.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                ServiceController service = new ServiceController(serviceName);
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    service.Stop();
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                }
+
+                service.Start();
+                service.WaitForStatus(ServiceControllerStatus.Running);
+                MessageBox.Show($"Serviço '{serviceName}' reiniciado com sucesso.", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao reiniciar o serviço: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
